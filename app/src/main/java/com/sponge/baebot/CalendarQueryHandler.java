7b2592a01c07b7362520300519d533ecec5cc1be
Zifeng Chen <32692755+zic079@users.noreply.github.com>
2018-11-02 23:20:13 -0700

    Working on calendar provider. Just finish the "Testing Mode" where no asynchronism handler been used. Also, didn't clean up code yet.

--- app/src/main/java/com/sponge/baebot/CalendarQueryHandler.java
+++ app/src/main/java/com/sponge/baebot/CalendarQueryHandler.java
@@ -1,13 +1,23 @@
+// https://developer.android.com/guide/topics/providers/calendar-provider
+// https://developer.android.com/reference/android/content/AsyncQueryHandler
+// https://billthefarmer.github.io/blog/adding-a-calendar-event/
 package com.sponge.baebot;
 
 import android.content.AsyncQueryHandler;
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CalendarContract;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.TimeZone;
 
 public class CalendarQueryHandler extends AsyncQueryHandler {
-    private static final String TAG = "QueryHandler";
+    private static final String TAG = "CalendarQueryHandler";
 
-    // From "Calendar provider overview"
     // Projection array. Creating indices for this array instead of doing
     // dynamic lookups improves performance.
     public static final String[] EVENT_PROJECTION = new String[] {
@@ -23,15 +33,173 @@ public class CalendarQueryHandler extends AsyncQueryHandler {
     private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
     private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
+    private static final int CALENDAR = 0;
+    private static final int EVENT    = 1;
+    private static final int REMINDER = 2;
+
     private static CalendarQueryHandler calendarQueryHandler;
+
+    private static ArrayList<String> calendarData = new ArrayList<>();
 
     // Public constructors: AsyncQueryHandler(ContentResolver cr)
     // https://developer.android.com/reference/android/content/AsyncQueryHandler
     // Constructor of the class
-    public CalendarQueryHandler(ContentResolver cr)
-    {
+    public CalendarQueryHandler(ContentResolver cr) {
         super(cr);
     }
 
+    @Override
+    public void onQueryComplete(int token, Object object, Cursor cursor) {
+        // Use the cursor to move through the returned records
+        //cursor.moveToFirst();
 
+        // Get the field values
+        ///long calendarID = cursor.getLong(PROJECTION_ID_INDEX);
+
+        while (cursor.moveToNext()) {
+            long calID = 0;
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
+
+            // Get the field values
+            calID = cursor.getLong(PROJECTION_ID_INDEX);
+            displayName = cursor.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cursor.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cursor.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+
+            // building string of current cursor data
+            String currentData = String.format("Calendar ID: %s\nDisplay Name: %s\nAccount Name: %s\nOwner Name: %s", calID, displayName, accountName, ownerName);
+            calendarData.add(currentData);
+
+            Log.d(TAG, currentData);
+        }
+
+        ///Log.d(TAG, "Calendar query complete " + calendarID);
+
+        /*
+        ContentValues values = (ContentValues) object;
+        values.put(CalendarContract.Events.CALENDAR_ID, calendarID);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE,
+                TimeZone.getDefault().getDisplayName());
+
+        startInsert(EVENT, null, CalendarContract.Events.CONTENT_URI, values);
+        */
+    }
+
+    @Override
+    public void onInsertComplete(int token, Object object, Uri uri) {
+        if (uri != null)
+        {
+
+            Log.d(TAG, "Insert complete " + uri.getLastPathSegment());
+
+            switch (token)
+            {
+                case EVENT:
+                    long eventID = Long.parseLong(uri.getLastPathSegment());
+                    ContentValues values = new ContentValues();
+                    values.put(CalendarContract.Reminders.MINUTES, 10);
+                    values.put(CalendarContract.Reminders.EVENT_ID, eventID);
+                    values.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+                    startInsert(REMINDER, null, CalendarContract.Reminders.CONTENT_URI, values);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onUpdateComplete(int token, Object cookie, int result) {
+        // update() completed
+    }
+
+    @Override
+    protected void onDeleteComplete(int token, Object cookie, int result) {
+        // delete() completed
+    }
+
+    public static ArrayList<String> readEvent(Context context) {
+
+        ContentResolver cr = context.getContentResolver();
+
+        // initialize the query handler if it didn't been initialized yet
+        if (calendarQueryHandler == null) {
+            calendarQueryHandler = new CalendarQueryHandler(cr);
+        }
+
+        // ContentResolver cr = context. getContentResolver();
+
+        // ---- current design return all calendar event (no filtering)
+        // could add the feature in the future - pass in from the parameter
+        /*
+        String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[] {"hera@example.com", "com.example",
+                "hera@example.com"};
+        */
+
+        // set selection and selectionArgs as null
+        String selection = null;
+        String[] selectionArgs = null;
+
+        //ArrayList<String> calendarData = new ArrayList<>();
+
+        //Cursor cur = cr.query(CalendarContract.Calendars.CONTENT_URI, EVENT_PROJECTION, selection, selectionArgs, null);
+        calendarQueryHandler.startQuery(CALENDAR, null, CalendarContract.Calendars.CONTENT_URI,
+                EVENT_PROJECTION, selection, selectionArgs, null);
+
+
+        /*
+        while (cur.moveToNext()) {
+            long calID = 0;
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
+
+            // Get the field values
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+
+            // building string of current cursor data
+            String currentData = String.format("Calendar ID: %s\nDisplay Name: %s\nAccount Name: %s\nOwner Name: %s", calID, displayName, accountName, ownerName);
+            calendarData.add(currentData);
+        }
+        */
+
+        //ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, calendarData);
+        //listView.setAdapter(stringArrayAdapter);
+
+        return calendarData;
+    }
+
+
+    // insertEvent method
+    public static void insertEvent(Context context, long startTime,
+                                   long endTime, String title, String description) {
+        ContentResolver cr = context.getContentResolver();
+
+        // initialize the query handler if it didn't been initialized yet
+        if (calendarQueryHandler == null) {
+            calendarQueryHandler = new CalendarQueryHandler(cr);
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.DTSTART, startTime);
+        values.put(CalendarContract.Events.DTEND, endTime);
+        values.put(CalendarContract.Events.TITLE, title);
+        values.put(CalendarContract.Events.DESCRIPTION, description);
+
+        // Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Calendar query start");
+        }
+
+        // start the query
+        calendarQueryHandler.startQuery(CALENDAR, values, CalendarContract.Calendars.CONTENT_URI,
+                EVENT_PROJECTION, null, null, null);
+    }
 }
--- app/src/main/java/com/sponge/baebot/MainActivity.java
+++ app/src/main/java/com/sponge/baebot/MainActivity.java
@@ -1,14 +1,208 @@
 package com.sponge.baebot;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // Projection array. Creating indices for this array instead of doing
+    // dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+
+    final String[] INSTANCE_PROJECTION = new String[] {
+            CalendarContract.Instances.EVENT_ID,      // 0
+            CalendarContract.Instances.BEGIN,         // 1
+            CalendarContract.Instances.TITLE          // 2
+    };
+
+
+
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
+    private static final int INSTANCE_ID_INDEX = 0;
+    private static final int INSTANCE_BEGIN_INDEX = 1;
+    private static final int INSTANCE_TITLE_INDEX = 2;
+
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // check permission
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            // checking permission before reading and writing calendar
+            if (checkSelfPermission(Manifest.permission.READ_CALENDAR)
+                    == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to read calendar");
+                String[] permissions = {Manifest.permission.READ_CALENDAR};
+                Log.d("permission", "requesting permission");
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+
+            if (checkSelfPermission(Manifest.permission.WRITE_CALENDAR)
+                    == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to write calendar");
+                String[] permissions = {Manifest.permission.WRITE_CALENDAR};
+                Log.d("permission", "requesting permission");
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
+
+        // read calendar data with AsyncQueryHandler
+        ArrayList<String> calendarData = new ArrayList<>();
+        calendarData = readEvent();
+
+        if(calendarData.size() > 0) {
+            TextView successText = (TextView)findViewById(R.id.event1);
+            successText.setText(calendarData.get(0));
+        }
+    }
+
+    ////// TESTING ONLY - NOT AsyncQueryHandler
+    private ArrayList<String> readEvent() {
+
+        // still making error debug message, move to onCreate seems to fix, don't know why
+        // but whatever..... it worked as for now
+        /*
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            // checking permission before reading and writing calendar
+            if (checkSelfPermission(Manifest.permission.READ_CALENDAR)
+                    == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to read calendar");
+                String[] permissions = {Manifest.permission.READ_CALENDAR};
+                Log.d("permission", "requesting permission");
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
+        */
+
+
+        // make up a time range for searching event
+        long startMillis = 0;
+        long endMillis = 0;
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2018, 11, 02, 6, 00);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2018, 11, 15, 6, 00);
+        endMillis = endTime.getTimeInMillis();
+
+
+        ContentResolver cr = getContentResolver();
+
+        // use CalendarContract.Instances for read data on calendar (rather than owner info)
+        //Uri uri = CalendarContract.Calendars.CONTENT_URI;
+        //Uri uri = CalendarContract.Instances.CONTENT_URI;
+        Uri CALENDAR_URI = Uri.parse("content://com.android.calendar/events");
+        /*
+        Uri.Builder builder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+        ContentUris.appendId(builder, startMillis);
+        ContentUris.appendId(builder, endMillis);
+        */
+
+        Cursor cur = null;
+
+        // ---- current design return all calendar event (no filtering)
+        // could add the feature in the future - pass in from the parameter
+        /*
+        String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[] {"hera@example.com", "com.example",
+                "hera@example.com"};
+        */
+
+        // set selection and selectionArgs as null
+        String selection = null;
+        String[] selectionArgs = null;
+
+        cur = cr.query(CALENDAR_URI, new String[] { "calendar_id", "title", "description",
+                "dtstart", "dtend", "eventLocation" }, selection, selectionArgs, null);
+        ArrayList<String> calendarData = new ArrayList<>();
+
+        if(cur.getCount() > 0) {
+            Log.d("readEvent", "events found");
+
+            cur.moveToFirst();
+            while (cur.moveToNext()) {
+                /*
+                long calID = 0;
+                String displayName = null;
+                String accountName = null;
+                String ownerName = null;
+
+                // Get the field values
+                calID = cur.getLong(PROJECTION_ID_INDEX);
+                displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+                accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+                ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+                */
+
+                /*
+                long calID = 0;
+                String eventBegin = null;
+                String eventTitle = null;
+
+                // Get the field values
+                calID = cur.getLong(INSTANCE_ID_INDEX);
+                eventBegin = cur.getString(INSTANCE_BEGIN_INDEX);
+                eventTitle = cur.getString(INSTANCE_TITLE_INDEX);
+                */
+
+                String eventName = null;
+                String eventBegin = null;
+                String eventDescription = null;
+
+                // Get the field values
+                eventName = cur.getString(1);
+                eventBegin = cur.getString(3);
+                eventDescription = cur.getString(2);
+
+
+                // building string of current cursor data
+                //String currentData = String.format("Calendar ID: %s\nDisplay Name: %s\nAccount Name: %s\nOwner Name: %s", calID, displayName, accountName, ownerName);
+                String currentData = String.format("Event Name: %s\nBegin Time: %s\nEvent Description: %s", eventName, eventBegin, eventDescription);
+                Log.d("readEvent", currentData);
+                calendarData.add(currentData);
+            }
+        }
+
+        //ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, calendarData);
+        //listView.setAdapter(stringArrayAdapter);
+
+        return calendarData;
     }
 }
+
