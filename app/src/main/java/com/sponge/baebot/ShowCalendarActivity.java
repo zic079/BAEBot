10934032a2f63f9289355e5b2fb0ccd28d125d9c
Cathy <cathy57401@126.com>
2018-11-25 23:36:06 -0800

    fullCalendar

--- app/src/main/java/com/sponge/baebot/ShowCalendarActivity.java
+++ app/src/main/java/com/sponge/baebot/ShowCalendarActivity.java
@@ -1,15 +1,21 @@
 package com.sponge.baebot;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
-public class ShowCalendarActivity extends AppCompatActivity
-        implements View.OnClickListener {
+public class ShowCalendarActivity extends AppCompatActivity {
 
 
     private static final String TAG = "CalendarActivity";
@@ -18,6 +24,8 @@ public class ShowCalendarActivity extends AppCompatActivity
 
     private TextView myDate;
 
+    private int inputYear, inputMonth, inputDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
@@ -25,17 +33,42 @@ public class ShowCalendarActivity extends AppCompatActivity
         setContentView(R.layout.activity_view_calendar);
         mCalendarView = (CalendarView) findViewById(R.id.calendarView);
         myDate = (TextView) findViewById(R.id.myDate);
+
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                String date = (year + 1) + "/" + month + "/" + dayOfMonth;
+                String date = year + "/" + (month+1) + "/" + dayOfMonth;
                 myDate.setText(date);
+                ArrayList<String> eventList = new ArrayList<>();
+                initRecyclerView(eventList);
+                if (year == 2018 && month+1 == 10 && dayOfMonth == 26) {
+                    eventList.add("find a boyfriend");
+                    eventList.add("review CSE110 quiz1");
+                    eventList.add("review CSE110 quiz2");
+                    eventList.add("review CSE110 quiz3");
+                    eventList.add("review CSE110 quiz4");
+                    eventList.add("review CSE110 quiz5");
+                    eventList.add("review CSE110 final");
+                    initRecyclerView(eventList);
+                }
+
             }
         });
-
     }
 
-    @Override
-    public void onClick(View v) {
+//    private boolean checkDate (int year, int month, int dayOfMonth) {
+//        String date = year + "/" + (month) + "/" + dayOfMonth;
+//        System.out.println(date);
+//        if (year == inputYear && month == inputMonth && dayOfMonth == inputDay)
+//            return true;
+//        return false;
+//    }
+
+    private void initRecyclerView(ArrayList<String> events) {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(events,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
