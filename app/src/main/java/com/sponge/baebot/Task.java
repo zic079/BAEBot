2d3ffd127d674021894d552968d62918fd413e6e
miyujian <miyujian12@gmail.com>
2018-11-27 12:12:18 -0800

    using timestamp

--- app/src/main/java/com/sponge/baebot/Task.java
+++ app/src/main/java/com/sponge/baebot/Task.java
@@ -2,6 +2,19 @@ package com.sponge.baebot;
 
 public class Task {
     private String title, description, taskId;
+    private long timestamp;
+
+
+    public Task(String taskId, String title, String description, long timestamp) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public void setTitle(String title) {
         this.title = title;
@@ -15,69 +28,23 @@ public class Task {
         this.taskId = taskId;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
 
-    public void setMonth(int month) {
-        this.month = month;
-    }
 
-    public void setDayOfMonth(int dayOfMonth) {
-        this.dayOfMonth = dayOfMonth;
-    }
-
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
-
-    private int year, month, dayOfMonth, hour, minute;
-
-    public Task(){ }
-    public Task(String taskId, String title, String description,int year, int month, int dayOfMonth,
-                int hour, int minute){
-        this.taskId = taskId;
-        this.title = title;
-        this.description = description;
-        this.year = year;
-        this.month = month;
-        this.dayOfMonth = dayOfMonth;
-        this.hour = hour;
-        this.minute = minute;
-    }
-    public  String getTaskId(){
+    public String getTaskId() {
         return taskId;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
 
-    public int getYear(){
-        return year;
+    public long getTimestamp() {
+        return timestamp;
     }
 
-    public int getMonth(){
-        return month;
-    }
 
-    public int getDayOfMonth(){
-        return dayOfMonth;
-    }
-
-    public int getHour(){
-        return hour;
-    }
-
-    public int getMinute(){
-        return minute;
-    }
 }
